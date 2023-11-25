ae23df5ea165ae2a89859419a0352bf5dc282016
shanruto <dalavanja025@gmail.com>
2023-11-25 19:09:17 +0100

    refactor: split config and organize files

---
+++ config/shared.lua
@@ -0,0 +1,24 @@
+return {
+    locations = {
+        ['apartment1'] = {
+            label = 'South Rockford Drive',
+            enter = vec4(-667.02, -1105.24, 14.63, 242.32)
+        },
+        ['apartment2'] = {
+            label = 'Morningwood Blvd',
+            enter = vec4(-1288.52, -430.51, 35.15, 124.81)
+        },
+        ['apartment3'] = {
+            label = 'Integrity Way',
+            enter = vec4(269.73, -640.75, 42.02, 249.07)
+        },
+        ['apartment4'] = {
+            label = 'Tinsel Towers',
+            enter = vec4(-619.29, 37.69, 43.59, 181.03)
+        },
+        ['apartment5'] = {
+            label = 'Fantastic Plaza',
+            enter = vec4(291.517, -1078.674, 29.405, 270.75)
+        },
+    }
+}
