0f97cd630481c142ff94791c88e36d7cd23d740b
stepan_valic <131240792+stepanvalic@users.noreply.github.com>
2024-03-29 18:21:38 +0100

    Fix cs.lua

--- locales/cs.lua
+++ locales/cs.lua
@@ -1,31 +1,30 @@
 local Translations = {
     error = {
-        to_far_from_door = 'Du bist zu weit von der Klingel entfernt',
-        nobody_home = 'Niemand ist zuhause..',
-        nobody_at_door = 'Niemand ist an der Tür...'
+        to_far_from_door = 'Jsi příliš daleko od zvonku',
+        nobody_home = 'Nikdo není doma..',
+        nobody_at_door = 'Nikdo není u dveří...'
     },
     success = {
-        receive_apart = 'Du hast eine Wohnung bekommen',
-        changed_apart = 'Du bist umgezogen',
+        receive_apart = 'Získal jsi byt',
+        changed_apart = 'Přestěhoval jsi se',
     },
     info = {
-        at_the_door = 'Jemand ist an der Tür!',
+        at_the_door = 'Někdo je u dveří!',
     },
     text = {
-        menu_header = 'Wohnungen',
-        door_outside = '[E] - Wohnungen',
-        enter = 'Wohnung betreten',
-        ring_doorbell = 'Klingeln',
-        logout = '[E] - Charakter ausloggen',
-        change_outfit = '[E] - Outfit ändern',
-        open_stash = '[E] - Stash öffnen',
-        move_here = 'Hierher ziehen',
-        open_door = 'Tür öffnen',
-        door_inside = '[E] - Tür',
-        leave = 'Wohnung verlassen',
-        close_menu = '⬅ Menü schließen',
-        tennants = 'Mieter',
-    },
+        menu_header = 'Byty',
+        door_outside = '[E] - Byty',
+        enter = 'Vstoupit do bytu',
+        ring_doorbell = 'Zazvonit',
+        logout = '[E] - Odhlásit postavu',
+        change_outfit = '[E] - Změnit oblečení',
+        open_stash = '[E] - Otevřít schránku',
+        move_here = 'Přestěhovat se sem',
+        open_door = 'Otevřít dveře',
+        door_inside = '[E] - Dveře',
+        leave = 'Opustit byt',
+        close_menu = '⬅ Zavřít menu',
+        tenants = 'Nájemníci',
 }
 
 if GetConvar('qb_locale', 'en') == 'cs' then
